f1cf09a3722d8707f22ea09e83a76e8b226cfdf6
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2021-12-08 21:51:07 -0500

    Completed path finding

--- Project6/make-plan.cpp
+++ Project6/make-plan.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stdexcept>
+#include <string>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 const int SIZE = 32;
@@ -11,21 +15,68 @@ void writeMap(int[SIZE][SIZE]);
 
 struct Node
 {
-    Node *parent;
+    const Node *parent;
     double gCost, hCost, fCost;
     int x, y, length;
 
-    Node(Node *parent, int x, int y, int goalX, int goalY, int cost) : parent(parent),
+    Node(const Node *parent, int x, int y, int goalX, int goalY, int cost) : parent(parent), x(x), y(y),
                                                              hCost(distance(x, y, goalX, goalY)),
                                                              gCost(cost + ((parent != nullptr) ? (distance(x, y, parent->x, parent->y) + parent->gCost) : 0)),
                                                              fCost(hCost + gCost),
                                                              length((parent != nullptr) ? (1 + parent->length) : 1){};
+    string to_string() const {
+        return "X: " + std::to_string(x) + " Y: " + std::to_string(y) + " G: " + std::to_string(gCost) + " H: " + std::to_string(hCost) + " F: " + std::to_string(fCost);
+    }
+    bool operator<(const Node& rhs) const{
+        return fCost < rhs.fCost;
+    }
+    bool operator==(const Node& rhs) const{
+        return x == rhs.x && y == rhs.y;
+    }
 };
-s
-vector<Node> getNeighbors(Node * parent, int map[SIZE][SIZE]);
+class MapRange{
+    int RealStart, RealEnd;
+    int MapStart, MapEnd;
+    double slope;
+    public:
+        MapRange(int RealStart, int RealEnd, int MapStart, int MapEnd):RealStart(RealStart),RealEnd(RealEnd),
+            MapStart(MapStart),MapEnd(MapEnd),
+            slope(1.0 * (RealEnd-RealStart)/(MapEnd-MapStart)){};
+        double operator()(double n){
+            return RealStart + slope * (n - MapStart);
+        }
+};
+vector<Node*> getNeighbors(const Node * parent, int map[SIZE][SIZE], int goalX, int goalY);
 int main(int argc, char const *argv[])
 {
-
+    int map[SIZE][SIZE];
+    MapRange MapPos(0, 31, -8, 8);
+    const int startX = ceil(MapPos(-6)); const int startY = ceil(MapPos(-6)); const int endX = ceil(MapPos(6.5)); const int endY = ceil(MapPos(6.5));
+    readMap(map);
+    priority_queue<Node*> open;
+    vector<const Node*> closed;
+    open.push(new Node(nullptr, startX, startY, endX, endY, 1));
+    // Node *startPos = new Node(nullptr, startX, startY, endX, endY, 1);
+    while (open.size() > 0)
+    {
+        const Node* currentNode = open.top();
+        cout << "Visiting Node: " << currentNode->to_string() << endl;
+        if(currentNode->x == endX && currentNode->y == endY){
+            cout << "Goal reached!" << endl;
+            break; //TODO: trace path
+        }
+        open.pop();
+        closed.push_back(currentNode);
+        for(Node* n : getNeighbors(currentNode, map, endX, endY)){
+            if (find(closed.begin(), closed.end(), n) != closed.end())
+                continue;
+            open.push(n);
+        }
+    }
+    
+    // cout << startPos->to_string() << endl;
+    // for (auto* n : getNeighbors(startPos, map, endX, endY))
+    //     cout << n->to_string() << endl;
     return 0;
 }
 double distance(int x, int y, int targetX, int targetY)
@@ -82,17 +133,21 @@ void readMap(int map[SIZE][SIZE])
 
     mapFile.close();
 }
-vector<Node> getNeighbors(Node * parent, int map[SIZE][SIZE]){
-    if (parent.x < 0 || parent.x >= SIZE || parent.y < 0 || parent.y >= SIZE)
-        throw invalid_argument;
-    vector<Node> neighbors;
+vector<Node*> getNeighbors(const Node * parent, int map[SIZE][SIZE], int goalX, int goalY){
+    if (parent->x < 0 || parent->x >= SIZE || parent->y < 0 || parent->y >= SIZE)
+        throw invalid_argument("Invalid argument out of bounds parent");
+    vector<Node*> neighbors;
     for (int x = -1; x < 2; x++){
         for (int y = -1; y < 2; y++){
             if (x == 0 && y == 0) continue;
             int xIndex = parent->x + x; int yIndex = parent->y + y;
             if (xIndex < 0 || xIndex >= SIZE || yIndex < 0 || yIndex >= SIZE)
                 continue;
-
+            if (map[xIndex][yIndex] != -1){
+                Node * n = new Node(parent, xIndex, yIndex, goalX, goalY, map[xIndex][yIndex]);
+                neighbors.push_back(n);
+            }
         }
     }
+    return neighbors;
 }
